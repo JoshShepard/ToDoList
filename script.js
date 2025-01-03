737d3340a5f55211e88659d0340efe8c7d3ff6b9
Joshua Shepard <joshua_shepard@yahoo.com>
2025-01-03 18:38:12 -0500

    First Commit - Played around with a couple of things. Next step - Add functionality to form, edit ul with task, REDO DESIGN/COLORS lul

---
+++ script.js
@@ -0,0 +1,7 @@
+// document.getElementById("header").innerHTML = "Test Interactivity using Javascript!";
+
+// let button = document.getElementById("testChange");
+
+// button.onclick = function() {
+//     document.getElementById("header").innerHTML = "Changed the header by using button!!!";
+// }
