ba31d4ec99f55d3c6a0f0cd065d56f56b97726d0
JoshShepard <joshua_shepard@yahoo.com>
2025-01-05 12:11:26 -0500

    Finish complete button logic, started working on edit button, close but still some issues

--- script.js
+++ script.js
@@ -4,11 +4,9 @@ let button = document.getElementById('submit');
 let inputField = document.getElementById('task');
 let todoList = document.getElementById('todo-list');
 
-
-
-// Event listener on submit button
+// Event listener on add button
 button.addEventListener('click', function() {
-    // removing whitespace
+    // removing whitespace off user input
     const task = inputField.value.trim();
 
     // HTML has required but check anyway
@@ -22,25 +20,60 @@ button.addEventListener('click', function() {
         editButton.textContent = 'EDIT';
         editButton.id = 'edit-button';
 
-        // newTask.appendChild(editButton);
-
+        // creates complete button
         const completeButton = document.createElement('button');
         completeButton.textContent = 'COMPLETE';
         completeButton.id = 'completed-button';
-
-        // newTask.appendChild(completeButton);
 
         // create a div to space buttons from task
         const buttonSection = document.createElement('div');
         buttonSection.appendChild(editButton);
         buttonSection.appendChild(completeButton);
 
+        // add button div to each task/list item
         newTask.appendChild(buttonSection);
 
+        // adds task to to-do list
         todoList.appendChild(newTask);
 
+        // resets input field for next task
         inputField.value = '';
 
+        completeButton.addEventListener('click', function() {
+            if (newTask.style.textDecoration === 'line-through') {
+                newTask.style.textDecoration = 'none';
+                newTask.style.backgroundColor = '#3f3f3f';
+                completeButton.textContent = 'COMPLETE';
+            } else {
+                newTask.style.textDecoration = 'line-through';
+                newTask.style.backgroundColor = '#D00000';
+                completeButton.textContent = 'UNDO';
+            }
+        });
+
+
+        // TODO FIX
+        // when edit is clicked, it shows buttons within inputfield 
+        // style the input field and save button
+        // after edit, buttons vanish on the task/list item
+        editButton.addEventListener('click', function() {
+            const originalText = newTask.textContent.replace("Edit", "").trim();
+            const editField = document.createElement('input');
+            editField.type = 'text';
+            editField.value = originalText;
+
+            newTask.innerHTML = "";
+            newTask.appendChild(editField);
+
+            const saveButton = document.createElement('button');
+            saveButton.textContent = 'SAVE';
+            buttonSection.appendChild(saveButton);
+            newTask.appendChild(buttonSection);
+
+            saveButton.addEventListener('click', function() {
+                newTask.textContent = editField.value;
+            })
+        });
     } else {
         alert('Enter a valid task!');
     }  
